13dc4df86615a0c223f27978e9589932f478562a
Thiago Zell Mendes <thiagosmendes.th@gmail.com>
2023-04-06 23:20:53 -0300

    Fix: StatusBarToLight

--- App.tsx
+++ App.tsx
@@ -14,6 +14,7 @@ import {
 import AppLoading from "expo-app-loading";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/global/styles/theme";
+import { StatusBar } from 'react-native';
 
 import { NavigationContainer } from "@react-navigation/native";
 import { AppRoutes } from "./src/routes/app.routes";
@@ -31,6 +32,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
+      <StatusBar barStyle='light-content'/>
         <AppRoutes />
       </NavigationContainer>
     </ThemeProvider>
